d309ffc4b938c0f72ba553e74c2d9fe56d50bf46
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-30 17:02:36 +0100

    Updates

---
+++ precache-manifest.777cd68b74b8c733f3c60d6f4aa29262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509b7989b69278196082bf2b437a6c83",
+    "url": "/mytest/index.html"
+  },
+  {
+    "revision": "38acbb573d514b8a845c",
+    "url": "/mytest/static/css/main.833e08f8.chunk.css"
+  },
+  {
+    "revision": "0b544181322c5c9e6ff3",
+    "url": "/mytest/static/js/2.f96aeebe.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/mytest/static/js/2.f96aeebe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38acbb573d514b8a845c",
+    "url": "/mytest/static/js/main.044f50c4.chunk.js"
+  },
+  {
+    "revision": "b4947aca59c71dab6d3d",
+    "url": "/mytest/static/js/runtime-main.4409ffe5.js"
+  }
+]);
